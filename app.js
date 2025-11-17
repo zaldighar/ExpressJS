00fb299234fab4b91ea521f0fcdfb6655eddd200
zaldighar <Zaldighar@outlook.com>
2025-11-17 14:44:41 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+require('dotenv').config()
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!')
+})
+
+
+const port = process.env.PORT || 3000
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
