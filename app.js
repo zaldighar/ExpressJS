88c9439063e68b412b9c17c1bcf40bc758672ced
zaldighar <Zaldighar@outlook.com>
2025-11-17 15:10:47 -0600

    add course API endpoints and sample data

--- app.js
+++ app.js
@@ -3,8 +3,28 @@ require('dotenv').config()
 
 const app = express()
 
+const courses = [
+  { id: 1, name: 'course1' },
+  { id: 2, name: 'course2' },
+  { id: 3, name: 'course3' },
+]
+
 app.get('/', (req, res) => {
   res.send('Hello, World!')
+})
+
+app.get('/api/courses', (req, res) => {
+  res.send([1, 2, 3])
+})
+
+app.get('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id))
+  if (!course) return res.status(404).send('The course with the given ID was not found.')
+  res.send(course.name)
+})
+
+app.get('/api/posts/:year/:month', (req, res) => {
+  res.send({ params: req.params, query: req.query })
 })
 
 
